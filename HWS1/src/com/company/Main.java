36809b1c53138bbb811267fd86eb6a1256e51343
ahg80 <ah.golrokhi@gmail.com>
2020-10-17 20:41:39 +0330

    first tamrin of first project

--- HWS1/src/com/company/Main.java
+++ HWS1/src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        float len,bre,peri,area;
+        Scanner scan =new Scanner(System.in);
+        System.out.println("plz enter the length and breadth of your shape");
+        len=scan.nextFloat();
+        bre=scan.nextFloat();
+        area=len*bre;
+        peri=(2*len)+(2*bre);
+        System.out.println("area = "+area);
+        System.out.println("primeter = "+peri);
     }
 }
+
