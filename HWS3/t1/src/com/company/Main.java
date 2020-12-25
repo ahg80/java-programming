ea291a11c4b18f8e5c6e7fd8d0904f6f271aa6da
ahg80 <71926467+ahg80@users.noreply.github.com>
2020-12-25 13:38:24 +0330

    hw3

---
+++ HWS3/t1/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            Student.input();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+
+
+    }
+
+}
+
---
+++ HWS3/t3/src/com/company/expetion.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class expetion {
+    public expetion() throws Exception {
+        Scanner input = new Scanner(System.in);
+        System.out.println(" Enter a string ");
+        java.lang.String str = input.nextLine();
+        if (str.matches(".*\\d.*")) {
+            throw new Exception("there are a number");
+        }
+        else
+            System.out.println("\"" + str.length() + "\" is the length of your string");
+
+    }
+}
